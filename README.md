78c21bfc04050f20482c072cdac7fe63693cba59
Keiko <47175457+KeikoPima@users.noreply.github.com>
2024-04-20 03:23:37 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# bugs-galore-template-1.20.4
+ Bugs mod
---
+++ src/client/java/keiko/bugsgalore/BugsGaloreClient.java
@@ -0,0 +1,10 @@
+package keiko.bugsgalore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BugsGaloreClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
