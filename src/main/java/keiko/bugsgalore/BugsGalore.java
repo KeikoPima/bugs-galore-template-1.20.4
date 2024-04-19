75b6c75477f2c552717a12910399f680abb50fb5
Keiko <47175457+KeikoPima@users.noreply.github.com>
2024-04-20 07:12:45 +1000

    Added Lang, Added Item group
    Currently have an error with a list type not classifying correctly not sure how to fix

--- src/main/java/keiko/bugsgalore/BugsGalore.java
+++ src/main/java/keiko/bugsgalore/BugsGalore.java
@@ -1,13 +1,21 @@
 package keiko.bugsgalore;
 
-import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.client.gui.tooltip.TooltipComponent;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
 
 public class BugsGalore implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -17,14 +25,27 @@ public class BugsGalore implements ModInitializer {
 
 	public static final Item BEETLE =
 		Registry.register(Registries.ITEM, new Identifier("bugsgalore", "beetle"),
-			new Item(new Item.Settings()));
+		new Item(new FabricItemSettings()));
+
+	public static final ItemGroup BUGS_GALORE = FabricItemGroup.builder()
+			.icon(() -> new ItemStack(BEETLE))
+			.displayName(Text.translatable("itemGroup.bugsgalore.bugsgaloreitemgroup"))
+			.entries((context, entires) -> {
+					entires.add(BEETLE);
+			})
+			.build();
 	
 	@Override
+	public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipComponent) {
+		tooltip.add(Text.translatable("item.bugsgalore.beetle.tooltip"));
+	}
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+
+		Registry.register(Registries.ITEM_GROUP, new Identifier("bugsgalore", "bugsgaloreitemgroup"), BUGS_GALORE);
 	}
 }
