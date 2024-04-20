c36abe3636654c0f077818e17e5b0ca17c87a714
Keiko <47175457+KeikoPima@users.noreply.github.com>
2024-04-21 03:20:54 +1000

    Fixing loot table

--- src/main/java/keiko/bugsgalore/BugsGalore.java
+++ src/main/java/keiko/bugsgalore/BugsGalore.java
@@ -8,14 +8,11 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.block.Blocks;
-import net.minecraft.entity.ItemEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
@@ -27,8 +24,11 @@ public class BugsGalore implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("bugsgalore");
 
+	// Loot table checker
 	private static final Identifier DIRT_LOOT_TABLE_ID = Blocks.DIRT.getLootTableId();
+	private static final Identifier GRASS_BLOCK_LOOT_TABLE_ID = Blocks.GRASS_BLOCK.getLootTableId();
  
+	// Item Registration
 	public static final Item BEETLE =
 		Registry.register(Registries.ITEM, new Identifier("bugsgalore", "beetle"),
 		new Item(new FabricItemSettings()));
@@ -37,6 +37,7 @@ public class BugsGalore implements ModInitializer {
 		Registry.register(Registries.ITEM, new Identifier("bugsgalore", "worms"),
 		new Item(new FabricItemSettings()));
 
+	// Itemgroup Creation
 	public static final ItemGroup BUGS_GALORE = FabricItemGroup.builder()
 			.icon(() -> new ItemStack(BEETLE))
 			.displayName(Text.translatable("itemGroup.bugsgalore.bugsgaloreitemgroup"))
@@ -45,7 +46,7 @@ public class BugsGalore implements ModInitializer {
 					entires.add(WORMS);
 			})
 			.build();
-	
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -54,15 +55,26 @@ public class BugsGalore implements ModInitializer {
 
 		LOGGER.info("Hello Fabric world!");
 
+		// Bellow are all loottable modifications
 		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
 			if (source.isBuiltin() && DIRT_LOOT_TABLE_ID.equals(id)) {
 				LootPool.Builder poolBuilder = LootPool.builder()
-						.with(ItemEntry.builder(Items.WORMS));
+						.with(ItemEntry.builder(WORMS));
 		 
 				tableBuilder.pool(poolBuilder);
 			}
 		});
 
+		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+			if (source.isBuiltin() && GRASS_BLOCK_LOOT_TABLE_ID.equals(id)) {
+				LootPool.Builder poolBuilder = LootPool.builder()
+						.with(ItemEntry.builder(WORMS));
+		 
+				tableBuilder.pool(poolBuilder);
+			}
+		});
+
+		// Item group registration
 		Registry.register(Registries.ITEM_GROUP, new Identifier("bugsgalore", "bugsgaloreitemgroup"), BUGS_GALORE);
 	}
 }
